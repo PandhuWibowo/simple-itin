d45a96801b9310d9b47b6b8a44d959d0c4aa9d7d
PandhuWibowo <pandhuw58@gmail.com>
2019-09-23 17:02:07 +0700

    table detail_tag telah ditulis

--- simple_itin/database/migrations/2019_09_23_092936_create_detail_tags_table.php
+++ simple_itin/database/migrations/2019_09_23_092936_create_detail_tags_table.php
@@ -14,8 +14,16 @@ class CreateDetailTagsTable extends Migration
     public function up()
     {
         Schema::create('detail_tags', function (Blueprint $table) {
-            $table->increments('id');
-            $table->timestamps();
+            $table->uuid("tag_id");
+            $table->uuid("wisata_id");
+            $table->string("nama_tag", 50)->nullable(false);
+//            $table->timestamps();
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+
+            $table->foreign("tag_id")->references("tag_id")->on("tags");
+            $table->foreign("wisata_id")->references("wisata_id")->on("wisatas");
         });
     }
 
