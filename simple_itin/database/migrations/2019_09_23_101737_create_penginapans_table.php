8598b1001d070654e0e17914e09694c099f50d5b
PandhuWibowo <pandhuw58@gmail.com>
2019-09-23 17:43:00 +0700

    master sudah selesai di buat

--- simple_itin/database/migrations/2019_09_23_101737_create_penginapans_table.php
+++ simple_itin/database/migrations/2019_09_23_101737_create_penginapans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateWisatasTable extends Migration
+class CreatePenginapansTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,22 +13,19 @@ class CreateWisatasTable extends Migration
      */
     public function up()
     {
-        Schema::create('wisatas', function (Blueprint $table) {
-            $table->uuid('wisata_id')->nullable(false)->primary();
-            $table->string("nama_wisata", 150)->nullable();
-            $table->uuid("kota_id")->nullable();
-            $table->text("alamat")->nullable();
-            $table->string("kontak", 15)->nullable();
+        Schema::create('penginapans', function (Blueprint $table) {
+            $table->uuid('penginapan_id')->nullable(false)->primary();
+//            $table->timestamps();
+            $table->string("nama_penginapan", 150)->nullable(false);
+            $table->uuid("kota_id");
             $table->time("waktu_operasional")->nullable();
             $table->enum("waktu_bagian", ['WIB','WIT','WITA'])->default('WIB');
-            $table->string("website", 150)->nullable();
             $table->longText("deskripsi")->nullable();
+            $table->text("alamat")->nullable();
             $table->string("image", 200)->nullable();
             $table->string("alt", 100)->nullable();
             $table->enum("is_approved", ['Publish','Reject','Pending'])->default("Pending");
             $table->string("url", "200")->nullable();
-//            $table->timestamps();
-
             $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
             $table->softDeletes();
@@ -44,6 +41,6 @@ class CreateWisatasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('wisatas');
+        Schema::dropIfExists('penginapans');
     }
 }
---
+++ simple_itin/database/migrations/2019_09_23_103244_create_detail_penginapans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetailPenginapansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_penginapans', function (Blueprint $table) {
+            $table->uuid('penginapan_id');
+            $table->uuid('jenis_penginapan_id');
+//            $table->timestamps();
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+
+            $table->foreign("penginapan_id")->references("penginapan_id")->on("penginapans");
+            $table->foreign("jenis_penginapan_id")->references("jenis_penginapan_id")->on("jenis_penginapans");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_penginapans');
+    }
+}
