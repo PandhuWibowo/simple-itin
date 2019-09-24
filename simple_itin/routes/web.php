27f0b88f5ff87b7dc22d064a6360927f7c569c45
PandhuWibowo <pandhuw58@gmail.com>
2019-09-24 14:18:20 +0700

    bikin halaman dashboard

--- simple_itin/routes/web.php
+++ simple_itin/routes/web.php
@@ -4,6 +4,7 @@ if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
     error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
 }
 
+//Kalau mau hapus cache di hosting
 Route::get('/clear-cache', function() {
     $exitCode = Artisan::call('config:clear');
     $exitCode = Artisan::call('cache:clear');
@@ -12,9 +13,26 @@ Route::get('/clear-cache', function() {
 });
 
 
-
+//Segala macam auth disini
 Auth::routes();
 
+//Halaman Home
+
+//Start
 Route::get('/backend/home', 'HomeController@index');
+
+Route::namespace("Dashboard")->group(function(){
+    Route::prefix("/backend")->group(function (){
+
+        //Bagian Dashboard Kota
+        Route::prefix("/cities")->group(function(){
+            Route::get("/","KotaController@index");
+        });
+
+        //Bagian Dashboard ...
+
+    });
+});
+//End
 
 
